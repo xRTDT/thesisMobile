deaec8baf7f56817929ea772f0ac35d8271d2362
Timothy Yoon <tim.keun.yoon@gmail.com>
2017-08-31 09:59:39 -0700

    Renders notes within a certain distance

--- LiveDontDie/LiveDontDie/ViewController.swift
+++ LiveDontDie/LiveDontDie/ViewController.swift
@@ -11,12 +11,13 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    var progress: Int = 0
+    var markers: Array<SCNNode>?
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var progressLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -25,11 +26,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/main.scn")!
-        let markers = Init.initMarkers(scene: scene)
-        print(markers)
-        
+        markers = Init.initMarkers(scene: scene)
+        print(markers!)
+        Init.calculateDistances(sceneView: sceneView, markers: markers!)
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        sceneView.addGestureRecognizer(tap)
+        
+        var timer = Timer()
+        
+        func scheduledTimerWithTimeInterval(){
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.timer), userInfo: nil, repeats: true)
+        }
+        
+
+        
+        scheduledTimerWithTimeInterval()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -78,5 +92,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    @objc func handleTap(_ gestureRecognizer: UIGestureRecognizer) {
+        let p = gestureRecognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(p, options: [:])
+        
+        if hitResults.count > 0 {
+            let node = hitResults[0].node
+            //make sure node is a note and not a marker
+            node.removeFromParentNode()
+            progress = progress + 1
+            let message = String(progress) + " of 8 notes collected."
+            animateNotification(message: message)
+        }
+    }
+    @objc func timer(){
+        let close = Init.calculateDistances(sceneView: sceneView, markers: markers!)
+        print(close)
+        if close {
+            animateNotification(message: "A note has appeared")
+        }
+    }
+    
+    func animateNotification(message: String) {
+        self.progressLabel.text = message
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveEaseOut, animations: {
+            self.progressLabel.alpha = 1.0
+        }, completion: {
+            (finished: Bool) -> Void in
+            UIView.animate(withDuration: 1.0, delay: 0.5, options: .curveEaseIn, animations: {
+                self.progressLabel.alpha = 0.0
+            })
+        })
     }
 }
