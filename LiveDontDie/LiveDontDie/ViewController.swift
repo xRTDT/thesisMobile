4cbfc6f49044b1d9e8bce1d6d6f00dbe497e14b1
rsong003 <richsong003@gmail.com>
2017-09-01 10:43:51 -0700

    Added DS_store file

--- LiveDontDie/LiveDontDie/ViewController.swift
+++ LiveDontDie/LiveDontDie/ViewController.swift
@@ -6,11 +6,78 @@
 //  Copyright © 2017 Timothy Yoon. All rights reserved.
 //
 
+import Mapbox
 import UIKit
 import SceneKit
 import ARKit
+import Mapbox
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class MBXCompassMapView: MGLMapView, MGLMapViewDelegate {
+    
+    var isMapInteractive : Bool = true {
+        didSet {
+            
+            // Disable individually, then add custom gesture recognizers as needed.
+            self.isZoomEnabled = false
+            self.isScrollEnabled = false
+            self.isPitchEnabled = false
+            self.isRotateEnabled = false
+        }
+    }
+    
+    // Create a map view and set the style.
+    override convenience init(frame: CGRect, styleURL: URL?) {
+        self.init(frame: frame)
+        self.styleURL = styleURL
+    }
+    
+    // Create a map view.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.delegate = self
+        self.alpha = 0.8
+        self.delegate = self
+        hideMapSubviews()
+    }
+    
+    // Make the map view a circle.
+    override func layoutSubviews() {
+        self.layer.cornerRadius = self.frame.width / 2
+    }
+    
+    // Hide the Mapbox wordmark, attribution button, and compass view. Move the attribution button and wordmark based on your design. See www.mapbox.com/help/how-attribution-works/#how-attribution-works for more information about attribution requirements.
+    private func hideMapSubviews() {
+        self.logoView.isHidden = true
+        self.attributionButton.isHidden = true
+        self.compassView.isHidden = true
+    }
+    
+    // Set the user tracking mode to `.followWithHeading`. This rotates the map based on the direction that the user is facing.
+    func mapViewWillStartLoadingMap(_ mapView: MGLMapView) {
+        self.userTrackingMode = .followWithHeading
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Adds a border to the map view.
+    func setMapViewBorderColorAndWidth(color: CGColor, width: CGFloat) {
+        self.layer.borderWidth = width
+        self.layer.borderColor = color
+    }
+}
+
+extension MBXCompassMapView {
+    func setupUserTrackingMode() {
+        self.showsUserLocation = true
+        self.setUserTrackingMode(.followWithHeading, animated: false)
+        self.displayHeadingCalibration = false
+    }
+}
+
+class ViewController: UIViewController, ARSCNViewDelegate, MGLMapViewDelegate {
+    var compass : MBXCompassMapView!
     var progress: Int = 0
     var markers: Array<SCNNode>?
     var monster: SCNNode?
@@ -20,6 +87,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        compass = MBXCompassMapView(frame: CGRect(x: 20,
+                                                  y: 20,
+                                                  width: view.bounds.width / 3,
+                                                  height: view.bounds.width / 3),
+                                    styleURL: URL(string: "mapbox://styles/jordankiley/cj5eeueie1bsa2rp4swgcteml"))
+        
+        compass.isMapInteractive = false
+        compass.tintColor = .black
+        compass.delegate = self
+        view.addSubview(compass)
         // Set the view's delegate
         sceneView.delegate = self
         
