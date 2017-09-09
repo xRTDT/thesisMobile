c7570790a9a9e8087433ebfcadec6432576af9b3
Timothy Yoon <tim.keun.yoon@gmail.com>
2017-09-09 15:15:41 -0700

    Adds sounds, rudimentary warning system

--- LiveDontDie/LiveDontDie/ViewController.swift
+++ LiveDontDie/LiveDontDie/ViewController.swift
@@ -11,14 +11,22 @@ import UIKit
 import SceneKit
 import ARKit
 import Mapbox
+import AVFoundation
 
 class ViewController: UIViewController, ARSCNViewDelegate, MGLMapViewDelegate {
+    
     var currentScore: Int = 0
     var compass : MBXCompassMapView!
     var progress: Int = 0
     var markers: Array<SCNNode>?
     var monster: SCNNode?
-    var monsterRange: Float = 30
+    var monsterRange: Float = 50
+    var BGMplayer: AVAudioPlayer!
+    var monsterPlayer: AVAudioPlayer!
+    var SFXplayer: AVAudioPlayer!
+    var monsterGotWithinrange: Bool = false
+    var isCloseToNote: Bool = false
+    
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var progressLabel: UILabel!
     
@@ -36,7 +44,17 @@ class ViewController: UIViewController, ARSCNViewDelegate, MGLMapViewDelegate {
         compass.delegate = self
         view.addSubview(compass)
         
-        
+//        AudioPlayer.playBGM()
+        let path = Bundle.main.path(forResource: "bgm", ofType: "WAV")!
+        let bgmURL = URL(fileURLWithPath: path)
+        do {
+            BGMplayer = try AVAudioPlayer(contentsOf: bgmURL)
+            BGMplayer.numberOfLoops = -1
+            BGMplayer.prepareToPlay()
+            BGMplayer.play()
+        } catch let error as NSError {
+            print(error.description)
+        }
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         sceneView.addGestureRecognizer(tap)
@@ -60,37 +78,88 @@ class ViewController: UIViewController, ARSCNViewDelegate, MGLMapViewDelegate {
         
         if hitResults.count > 0 {
             let node = hitResults[0].node
-            progress = progress + 1
-            let message = String(progress) + " of 8 notes collected."
-            Animations.displayNotification(message: message, label: progressLabel)
-            Animations.grab(node: node, sceneView: sceneView)
+            if node.opacity == 1 {
+                progress = progress + 1
+                let message = String(progress) + " of 8 notes collected."
+                Animations.displayNotification(message: message, label: progressLabel)
+                Animations.grab(node: node, sceneView: sceneView)
+                let path = Bundle.main.path(forResource: "pickup", ofType: "wav")!
+                let sfxURL = URL(fileURLWithPath: path)
+                do {
+                    SFXplayer = try AVAudioPlayer(contentsOf: sfxURL)
+                    SFXplayer.prepareToPlay()
+                    SFXplayer.play()
+                } catch let error as NSError {
+                    print(error.description)
+                }
+            }
         }
     }
     
     @objc func FrameTimer(){
+        var closestNote: Float?
         
         for node in markers! {
             let markerDistance = Init.calculateDistance(sceneView: sceneView, node: node)
-            if markerDistance < 5 {
-                Animations.displayNotification(message: "You are close to a note", label: progressLabel)
-                if markerDistance < 3 {
-                    Init.renderNote(sceneView: sceneView, node: node)
-                }
+            if markerDistance < closestNote! || closestNote == nil {
+                closestNote = markerDistance
             }
         }
+        
+        if closestNote! < 15 && !isCloseToNote {
+            isCloseToNote = true
+            self.progressLabel.text = "You are close to a key"
+            UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveEaseOut, animations: {
+                self.progressLabel.alpha = 1.0
+            })
+        }
+        
+        if closestNote! < 5 {
+            UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveEaseOut, animations: {
+                self.progressLabel.alpha = 0.0
+            }, completion: {
+                (finished: Bool) -> Void in
+                Animations.displayNotification(message: "A key has appeared", label: self.progressLabel)
+            })
+        }
+        
+        if closestNote! > 15 && isCloseToNote {
+            UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveEaseOut, animations: {
+                self.progressLabel.alpha = 0.0
+            })
+        }
+        
+        
+        
         if monster != nil {
             if(sceneView.scene.rootNode.childNode(withName: monster!.name!, recursively: true) != nil){
                 let monsterDistance = Init.calculateDistance(sceneView: sceneView, node: monster!)
                 //fade out monster at large distances
-                if monsterDistance > 30 && monster!.opacity == 1 {
+                if monsterDistance > 45 && monster!.opacity == 1 {
                     Animations.fadeOut(node: monster!)
+                    SFXplayer.stop()
                 } else if monsterDistance < 30 && monster!.opacity == 0 {
+                    monsterGotWithinrange = true
                     Animations.fadeIn(node: monster!)
+                    print("monster has faded in")
+                    let path = Bundle.main.path(forResource: "monsterClose", ofType: "wav")!
+                    let sfxURL = URL(fileURLWithPath: path)
+                    do {
+                        monsterPlayer = try AVAudioPlayer(contentsOf: sfxURL)
+                        monsterPlayer.prepareToPlay()
+                        monsterPlayer.numberOfLoops = -1
+                        monsterPlayer.play()
+                    } catch let error as NSError {
+                        print(error.description)
+                    }
+                    
                 }
                 
-                //remove monster from game is player is too far from monster
-                if monsterDistance > 35 {
+
+                if monsterDistance > 50 && monsterGotWithinrange {
+                    print("monster unrendered")
                     monster?.removeFromParentNode()
+                    monsterGotWithinrange = false
                 }
                 
                 if monsterDistance < 3 {
